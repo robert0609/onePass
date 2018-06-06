802312d56ee4d302119bcc4e7794f4a5b11af33a
robert0609 <robert0609@163.com>
2018-06-06 14:58:21 +0800

    站点列表

---
+++ app/src/main/java/com/bluefox/tool/onepass/SiteListActivity.java
@@ -0,0 +1,13 @@
+package com.bluefox.tool.onepass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SiteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_site_list);
+    }
+}
